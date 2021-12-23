f3600d0f453f7b6d8a5a7641ddfd20ff7ceb69ab
plug <plug185@gmail.com>
2021-12-23 18:59:14 +0700

    Lesson: Read blog

--- blog/blog_client/client.go
+++ blog/blog_client/client.go
@@ -40,10 +40,22 @@ func main() {
 		log.Fatalf("Unexpected error: %v", err)
 	}
 	fmt.Println("Blog has been created: %v", createBlogRes)
-	// doServerStreaming(c)
-	// doClientStreaming(c)
-	// doBiDiStreaming(c)
 
-	// doUnaryWithDeadline(c, 5*time.Second) // should complete
-	// doUnaryWithDeadline(c, 1*time.Second) // should timeout
+	blogId := createBlogRes.GetBlog().GetId()
+
+	// read blog
+	fmt.Println("reading the blog.")
+
+	_, err2 := c.ReadBlog(context.Background(), &blogpb.ReadBlogRequest{BlogId: "61c46457d7d796381a59580c"})
+	if err2 != nil {
+		fmt.Println("Error happened while reading2: %v", err2)
+	}
+
+	readBlogReq := &blogpb.ReadBlogRequest{BlogId: blogId}
+	readBlogres, readBlogErr := c.ReadBlog(context.Background(), readBlogReq)
+	if readBlogErr != nil {
+		fmt.Println("Error happened while reading: %v", readBlogErr)
+	}
+	fmt.Println("Blog was read: %v", readBlogres)
+
 }
