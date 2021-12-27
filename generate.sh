9c68166eda533cce9d8a0a453006ec1841d3d8c9
plug <plug185@gmail.com>
2021-12-27 10:27:15 +0700

    Add envoy proxy connect FE/BE

--- generate.sh
+++ generate.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
+# Go server
 protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative blog/blogpb/blog.proto
+
+
+# JS server
+protoc -I=blog/blogpb/ blog.proto --js_out=import_style=commonjs:blog/blogpb/web_client/
+# JS client
+protoc -I=blog/blogpb/ blog.proto --grpc-web_out=import_style=commonjs,mode=grpcwebtext:blog/blogpb/web_client/
+
+
+
+# For web client (FE/BE same architecture)
+protoc -I=blog/blogpb/ blog.proto --js_out=import_style=commonjs,binary:blog/jsclient/src/ --grpc-web_out=import_style=commonjs,mode=grpcwebtext:blog/jsclient/src/
+
+# Example in web
+# protoc --proto_path=todo --js_out=import_style=commonjs,binary:todo-client/src/ --grpc-web_out=import_style=commonjs,mode=grpcwebtext:todo-client/src/ todo/todo.proto
